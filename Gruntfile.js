70b8ed98a70c69a3188a05cddd6cf37bb654f424
Sathish Kumar Thangaraj <thangaraj@thinksoon-lm.corp.yahoo.com>
2015-12-16 14:46:39 -0800

    updated project structure

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt){
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json");
+		concat:{
+			dist:{
+				src: ["/vendor/reset-css/reset.css", "/vendor/bootstrap/dist/bootstrap.css"],
+				dest: ["dist/app.css"]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.registerTask("default", []);
+};
